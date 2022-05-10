6ae2e1f1d65465669f33e3a0a3c0c0dd17c7f94b
charlotte-greatbatch <charlottegreatbatch01@gmail.com>
2022-05-10 17:45:50 +1200

    half way through mole

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -1,5 +1,21 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+console.log('Whack-a-Mole!')
 
-console.log("Whack-a-Mole!")
+let theCells = document.getElementsByTagName('TD')
+//for (let i = 0; i < theCells.length; i++) {
+//  theCells[i].onclick = cellClicked
+//}
+function randomMole(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min
+}
+let randomIndex = randomMole(0, 25)
+let randomCell = theCells[randomIndex]
+
+function molePic() {
+  const img = document.createElement('img')
+  img.src = 'mole.PNG'
+  randomCell.appendChild(img)
+}
+molePic(randomCell)
